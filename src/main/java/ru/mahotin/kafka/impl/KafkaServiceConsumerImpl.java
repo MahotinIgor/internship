de1cb6095f6a8b1ddda81de7bb858666bb84407a
Makhotin Igor <mahotinigor@gmail.com>
2024-12-25 21:49:07 +0700

    refactor: add ack and pull any records from broker

--- src/main/java/ru/mahotin/kafka/impl/KafkaServiceConsumerImpl.java
+++ src/main/java/ru/mahotin/kafka/impl/KafkaServiceConsumerImpl.java
@@ -1,8 +1,9 @@
 package ru.mahotin.kafka.impl;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Service;
 import ru.mahotin.kafka.KafkaServiceConsumer;
 import ru.mahotin.service.MailService;
@@ -22,21 +23,27 @@ public class KafkaServiceConsumerImpl implements KafkaServiceConsumer {
 
     @KafkaListener(groupId = "group-1", topics = "status-topic")
     @Override
-    public void listenGroup1(StatusTaskDTO message) {
+    public void listenGroup1(ConsumerRecord<String, StatusTaskDTO[]> record,
+                             Acknowledgment ack
+    ) {
         String subject;
         String text;
-        log.info("Received message from kafka = {}", message);
-
-        if(message.taskid() < 0) {
-            subject = "Error when update status task";
-            text = "Null received at deserializing to StatusTaskDTO";
-        } else {
-            subject = String.format("Status task id = %d updated",message.taskid());
-            text = String.format("Task id = %d, new status = %s",
-                    message.taskid(),
-                    message.newStatus()
-            );
+        StatusTaskDTO[] messages = record.value();
+        for(StatusTaskDTO message : messages) {
+            log.info("Received message from kafka = {}", message);
+            if (message.taskid() < 0) {
+                subject = "Error when update status task";
+                text = "Null received at deserializing to StatusTaskDTO";
+            } else {
+                subject = String.format("Status task id = %d updated", message.taskid());
+                text = String.format("Task id = %d, new status = %s",
+                        message.taskid(),
+                        message.newStatus()
+                );
+            }
+            mailService.sendSimpleMessage(props.getUsername(), subject, text);
         }
-        mailService.sendSimpleMessage(props.getUsername(), subject,text);
+        ack.acknowledge();
+
     }
 }
